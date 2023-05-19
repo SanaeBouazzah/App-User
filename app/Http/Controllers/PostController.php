d576edf15b0a54a99e87c8de0a34564794616133
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-19 22:41:19 +0200

    commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function createPost()
+    public function createPost(Request $request)
     {
-
+      $incomingFields = $request->validate();
     }
 }
