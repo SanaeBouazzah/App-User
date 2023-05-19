279ff637a5fe600e852938911f3ba56d1e6e0464
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-19 21:50:07 +0200

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,9 @@ class UserController extends Controller
       'loginname'=>'required',
       'loginpassword'=>'required'
     ]);
-    if
+    if(auth()->attempt(['name'=> $incomingFields['loginname'], ['password'=> $incomingFields['loginpassword']]])){
+      $request->session()->regenerate();
+    }
     return redirect('/');
   }
    
