69d00dc4b8a4952bffc160dd511bbc034715de13
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-19 21:32:50 +0200

    re

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/', function () {
     return view('home');
 });
 Route::post('/register', [UserController::class, 'register']);
-Route::post('/logout',[])
+Route::post('/logout',[UserController::class, 'logout']);
