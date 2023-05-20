ec23de88640350a31cff9871239c69deeb8b545d
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-20 16:09:48 +0200

    commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function DeletePost(Post $post)
     {
       if(auth()->user()->id === $post['user_id']){
-        
+        $post->delete();
       }
       return redirect('/');
     }
