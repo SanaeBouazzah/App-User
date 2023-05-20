19aadd131f2150d5a334026f13c49fc98dd8316c
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-20 16:27:16 +0200

    commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -13,6 +14,6 @@ class Post extends Model
 
     public function user()
     {
-      
+      return $this->belongsTo(User::class);
     }
 }
